7ac17e5d48c6e8950793ed6f1b5c1290baf6c015
griv000 <57643552+griv000@users.noreply.github.com>
2021-11-05 08:53:57 -0400

    Updated with Solution

--- music.sql
+++ music.sql
@@ -1,1 +1,19 @@
+CREATE TABLE Artists (
+    artistID INTEGER PRIMARY KEY,
+    artistFirstName TEXT,
+    artistLastName TEXT
+);
 
+CREATE TABLE Albums (
+    albumID INTEGER PRIMARY KEY,
+    albumName TEXT,
+    ref_ArtistID INTEGER
+);
+
+CREATE TABLE Songs (
+    songID INTEGER PRIMARY KEY,
+    songName TEXT,
+    trackNum INTEGER,
+    songDuration INTEGER,
+    ref_AlbumID INTEGER
+);
